c22f451b7f9809b99d12caba5e0b41be06c814fa
J-J-T-M <jonatham.junior.18@gmail.com>
2026-01-26 19:45:58 -0400

    feat: Implementar controlador de autenticação com endpoints para registro e login de usuários

---
+++ src/main/java/com/jonathamjtm/gestaoartistas/dto/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.jonathamjtm.gestaoartistas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
