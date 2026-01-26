3661b8222b4d55cc059daf21f1e477cdae209734
J-J-T-M <jonatham.junior.18@gmail.com>
2026-01-26 19:46:30 -0400

    feat: Adicionar classe RegisterRequest para validação de dados de registro

---
+++ src/main/java/com/jonathamjtm/gestaoartistas/dto/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.jonathamjtm.gestaoartistas.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotBlank(message = "O nome completo é obrigatório")
+    private String fullName;
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Formato de e-mail inválido")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+    private String password;
+}
