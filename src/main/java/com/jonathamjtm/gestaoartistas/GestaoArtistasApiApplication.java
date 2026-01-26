06e9035924c88b37b03075cb08003f3f90af2b21
J-J-T-M <jonatham.junior.18@gmail.com>
2026-01-26 18:13:59 -0400

    feat: Inicialize a aplicação Spring Boot com os arquivos de instalação e configuração do Maven.

---
+++ src/main/java/com/jonathamjtm/gestaoartistas/GestaoArtistasApiApplication.java
@@ -0,0 +1,13 @@
+package com.jonathamjtm.gestaoartistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoArtistasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoArtistasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathamjtm/gestaoartistas/GestaoArtistasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathamjtm.gestaoartistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoArtistasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
