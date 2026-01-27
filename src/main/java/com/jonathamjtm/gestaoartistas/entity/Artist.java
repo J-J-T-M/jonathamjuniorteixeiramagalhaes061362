f7c0b8dc05cc9df2ebc542c3292c55c915760f43
J-J-T-M <jonatham.junior.18@gmail.com>
2026-01-27 11:39:04 -0400

    feat: Adicionar entidade Artist com mapeamento para álbuns e timestamps

---
+++ src/main/java/com/jonathamjtm/gestaoartistas/entity/Artist.java
@@ -0,0 +1,42 @@
+package com.jonathamjtm.gestaoartistas.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "artists")
+public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "artists", fetch = FetchType.LAZY)
+    @JsonIgnoreProperties("artists")
+    private Set<Album> albums = new HashSet<>();
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
