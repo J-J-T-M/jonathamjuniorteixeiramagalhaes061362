c1d6dccd4d0c89cf3ed5c1a3497a49af0501a789
J-J-T-M <jonatham.junior.18@gmail.com>
2026-01-27 17:36:45 -0400

    feat: Habilitar agendamento de tarefas com a anotação @EnableScheduling

--- src/main/java/com/jonathamjtm/gestaoartistas/GestaoArtistasApiApplication.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/GestaoArtistasApiApplication.java
@@ -2,8 +2,10 @@ package com.jonathamjtm.gestaoartistas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GestaoArtistasApiApplication {
 
 	public static void main(String[] args) {
