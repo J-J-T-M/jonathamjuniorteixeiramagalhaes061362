3f781583764f1ff32631482c4cf4dca69c2c9657
J-J-T-M <jonatham.junior.18@gmail.com>
2026-01-28 15:05:35 -0400

    feat: Adicionar agendador para sincronização regional com execução programada

---
+++ src/main/java/com/jonathamjtm/gestaoartistas/scheduler/RegionalScheduler.java
@@ -0,0 +1,29 @@
+package com.jonathamjtm.gestaoartistas.scheduler;
+
+import com.jonathamjtm.gestaoartistas.service.RegionalSyncService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class RegionalScheduler {
+
+    private final RegionalSyncService regionalSyncService;
+
+    @Scheduled(fixedRateString = "${integration.regionais.fixed-rate}")
+    public void scheduleSync() {
+        log.info("Agendador disparou o job de sincronização...");
+        regionalSyncService.executeSyncJob();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onStartup() {
+        log.info("Aplicação iniciada! Disparando job inicial em background...");
+        regionalSyncService.executeSyncJob();
+    }
+}
