c2fb3a7d411eae0d32a4d9373a2d341647bbc322
J-J-T-M <jonatham.junior.18@gmail.com>
2026-01-28 16:18:57 -0400

    feat: Adicionar suporte a paginação e filtragem por artista no repositório de álbuns

--- src/main/java/com/jonathamjtm/gestaoartistas/repository/AlbumRepository.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/repository/AlbumRepository.java
@@ -1,12 +1,17 @@
 package com.jonathamjtm.gestaoartistas.repository;
 
 import com.jonathamjtm.gestaoartistas.entity.Album;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface AlbumRepository extends JpaRepository<Album, Long> {
+public interface AlbumRepository extends JpaRepository<Album, Long>, JpaSpecificationExecutor<Album> {
 
     Optional<Album> findByTitle(String title);
+
+    Page<Album> findByArtists_Id(Long artistId, Pageable pageable);
 
 }
