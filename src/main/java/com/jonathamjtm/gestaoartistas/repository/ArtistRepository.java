5679246e6ba883a2017312dbc8ed71e8b23cc92b
J-J-T-M <jonatham.junior.18@gmail.com>
2026-01-28 16:19:40 -0400

    feat: Adicionar suporte a especificações e ordenação na busca de artistas

--- src/main/java/com/jonathamjtm/gestaoartistas/repository/ArtistRepository.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/repository/ArtistRepository.java
@@ -1,12 +1,12 @@
 package com.jonathamjtm.gestaoartistas.repository;
 
 import com.jonathamjtm.gestaoartistas.entity.Artist;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import java.util.List;
 
-import java.util.Optional;
+public interface ArtistRepository extends JpaRepository<Artist, Long>, JpaSpecificationExecutor<Artist> {
 
-public interface ArtistRepository extends JpaRepository<Artist, Long> {
-
-    Optional<Artist> findByName(String name);
-
+    List<Artist> findByNameContainingIgnoreCase(String name, Sort sort);
 }
