7bb035f39f8261e69f3ed07dec7c1ff9628676df
J-J-T-M <jonatham.junior.18@gmail.com>
2026-01-28 17:05:33 -0400

    feat: Adicionar suporte à paginação na busca de regionais ativos

--- src/main/java/com/jonathamjtm/gestaoartistas/repository/RegionalRepository.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/repository/RegionalRepository.java
@@ -1,12 +1,15 @@
 package com.jonathamjtm.gestaoartistas.repository;
 
 import com.jonathamjtm.gestaoartistas.entity.Regional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RegionalRepository extends JpaRepository<Regional, Long> {
 
     List<Regional> findByActiveTrue();
+
+    Page<Regional> findByActive(Boolean active, Pageable pageable);
 }
