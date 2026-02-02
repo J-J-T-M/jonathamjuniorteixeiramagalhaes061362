ecf4af661a0c9b0fc0fd94889ebb99d75ebba0cc
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-02 01:33:17 -0400

    feat: Adicionar método para buscar imagem de álbum por ID do álbum

--- src/main/java/com/jonathamjtm/gestaoartistas/repository/AlbumImageRepository.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/repository/AlbumImageRepository.java
@@ -3,5 +3,9 @@ package com.jonathamjtm.gestaoartistas.repository;
 import com.jonathamjtm.gestaoartistas.entity.AlbumImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AlbumImageRepository extends JpaRepository<AlbumImage, Long> {
+
+    Optional<AlbumImage> findByAlbumId(Long albumId);
 }
