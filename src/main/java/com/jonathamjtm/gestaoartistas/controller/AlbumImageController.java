9f5e31675626eb9b4501cbb95d9db6d4f5171cc4
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-01 23:54:44 -0400

    feat: Atualizar AlbumImageController para usar FileStorageService e alterar endpoint de upload de imagem

--- src/main/java/com/jonathamjtm/gestaoartistas/controller/AlbumImageController.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/controller/AlbumImageController.java
@@ -1,6 +1,6 @@
 package com.jonathamjtm.gestaoartistas.controller;
 
-import com.jonathamjtm.gestaoartistas.service.AlbumService;
+import com.jonathamjtm.gestaoartistas.service.storage.FileStorageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,21 +11,18 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/v1/albums")
+@RequestMapping("/api/v1/albums") //
 @RequiredArgsConstructor
-@Tag(name = "Álbuns - Imagens", description = "Upload de capas")
+@Tag(name = "Álbuns", description = "Gerenciamento de capas")
 @SecurityRequirement(name = "bearer-key")
 public class AlbumImageController {
 
-    private final AlbumService albumService;
+    private final FileStorageService fileStorageService;
 
-    @PostMapping(value = "/{id}/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @Operation(summary = "Upload de Capa", description = "Envia uma imagem (jpg/png) para o álbum. Salva no MinIO.")
-    public ResponseEntity<String> uploadImage(
-            @PathVariable Long id,
-            @RequestPart("file") MultipartFile file) {
-
-        albumService.uploadAlbumCover(id, file);
-        return ResponseEntity.ok("Upload realizado com sucesso!");
+    @PostMapping(value = "/{id}/cover", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Upload de Capa", description = "Envia a imagem da capa do álbum")
+    public ResponseEntity<String> uploadCover(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
+        String fileName = fileStorageService.upload(file);
+        return ResponseEntity.ok("Imagem salva: " + fileName);
     }
 }
