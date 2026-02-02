9fdcca01495d8ff5a303ca9cc6d9254ee72a8893
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-02 16:56:03 -0400

    feat: Adicionar classe RefreshTokenRequest para manipulação de tokens de refresh

---
+++ src/main/java/com/jonathamjtm/gestaoartistas/dto/auth/RefreshTokenRequest.java
@@ -0,0 +1,14 @@
+package com.jonathamjtm.gestaoartistas.dto.auth;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenRequest {
+    @NotBlank(message = "O Refresh Token é obrigatório")
+    private String refreshToken;
+}
