0929cbe33f63c8832fc6f6b21fc33f44d15d9240
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-02 01:35:06 -0400

    feat: Refatorar FileStorageService para criar uma interface e definir os métodos de upload, getPresignedUrl e delete.

--- src/main/java/com/jonathamjtm/gestaoartistas/service/storage/FileStorageService.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/service/storage/FileStorageService.java
@@ -1,69 +1,12 @@
 package com.jonathamjtm.gestaoartistas.service.storage;
 
-import io.minio.GetPresignedObjectUrlArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.minio.http.Method;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.InputStream;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
+public interface FileStorageService {
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class FileStorageService {
+    String upload(MultipartFile file);
 
-    private final MinioClient minioClient;
+    String getPresignedUrl(String fileName);
 
-    @Value("${integration.minio.bucket-name:capas-albuns}")
-    private String bucketName;
-
-    /**
-     * Faz o upload do arquivo e retorna o nome gerado (UUID)
-     */
-    public String upload(MultipartFile file) {
-        try {
-            String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
-            InputStream inputStream = file.getInputStream();
-
-            minioClient.putObject(
-                    PutObjectArgs.builder()
-                            .bucket(bucketName)
-                            .object(fileName)
-                            .stream(inputStream, file.getSize(), -1)
-                            .contentType(file.getContentType())
-                            .build()
-            );
-
-            return fileName;
-        } catch (Exception e) {
-            log.error("Erro ao enviar arquivo para MinIO", e);
-            throw new RuntimeException("Erro ao processar upload de imagem.");
-        }
-    }
-
-    /**
-     * Gera URL temporária para visualização (Presigned URL)
-     */
-    public String generatePresignedUrl(String objectName) {
-        try {
-            return minioClient.getPresignedObjectUrl(
-                    GetPresignedObjectUrlArgs.builder()
-                            .method(Method.GET)
-                            .bucket(bucketName)
-                            .object(objectName)
-                            .expiry(30, TimeUnit.MINUTES) // Requisito do Edital: 30 min
-                            .build()
-            );
-        } catch (Exception e) {
-            log.error("Erro ao gerar URL assinada", e);
-            return null;
-        }
-    }
+    void delete(String fileName);
 }
