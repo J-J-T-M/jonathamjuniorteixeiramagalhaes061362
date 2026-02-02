67121b01472baa68754c62f6a73331b8fff1b0db
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-02 14:25:07 -0400

    feat: Adicionar método de repositório para encontrar regiões ativas por ID externo

--- src/main/java/com/jonathamjtm/gestaoartistas/repository/RegionalRepository.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/repository/RegionalRepository.java
@@ -4,12 +4,17 @@ import com.jonathamjtm.gestaoartistas.entity.Regional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface RegionalRepository extends JpaRepository<Regional, Long> {
+
+    Page<Regional> findByActive(Boolean active, Pageable pageable);
 
     List<Regional> findByActiveTrue();
 
-    Page<Regional> findByActive(Boolean active, Pageable pageable);
+    Optional<Regional> findByExternalIdAndActiveTrue(Long externalId);
 }
