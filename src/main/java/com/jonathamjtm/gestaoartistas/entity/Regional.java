8ba46564bd9ed23ac6aa7f6e85622d0b4d9eb15f
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-02 14:23:01 -0400

    feat: Refactor Regional entity to update field names and simplify annotations

--- src/main/java/com/jonathamjtm/gestaoartistas/entity/Regional.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/entity/Regional.java
@@ -1,34 +1,27 @@
 package com.jonathamjtm.gestaoartistas.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.UpdateTimestamp;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "regionals")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "regionals")
 public class Regional {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "regional_id", nullable = false)
-    private Long regionalId;
+    @Column(name = "external_id", nullable = false)
+    private Long externalId;
 
     @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
-    private boolean active;
-
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
+    private Boolean active = true;
 }
