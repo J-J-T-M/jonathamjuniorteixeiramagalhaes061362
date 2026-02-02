b6980e8af634dc8e35d19c533c1b9927686ef447
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-01 23:02:04 -0400

    feat: Adicionar lista de álbuns na resposta do artista

--- src/main/java/com/jonathamjtm/gestaoartistas/dto/ArtistResponse.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/dto/ArtistResponse.java
@@ -1,15 +1,29 @@
 package com.jonathamjtm.gestaoartistas.dto;
 
+import com.jonathamjtm.gestaoartistas.entity.Album;
 import com.jonathamjtm.gestaoartistas.entity.Artist;
 import lombok.Data;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class ArtistResponse {
     private Long id;
     private String name;
+    private List<String> albums;
 
     public ArtistResponse(Artist artist) {
         this.id = artist.getId();
         this.name = artist.getName();
+
+        if (artist.getAlbums() != null) {
+            this.albums = artist.getAlbums().stream()
+                    .map(Album::getTitle)
+                    .collect(Collectors.toList());
+        } else {
+            this.albums = Collections.emptyList();
+        }
     }
 }
