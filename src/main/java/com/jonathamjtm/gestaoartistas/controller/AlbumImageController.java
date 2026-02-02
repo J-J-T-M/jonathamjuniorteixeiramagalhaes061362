ff68ffaaccdb6c7b8960b750ef394781b4edd964
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-02 01:32:48 -0400

    feat: Adicionar endpoint para obter URL da capa do álbum

--- src/main/java/com/jonathamjtm/gestaoartistas/controller/AlbumImageController.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/controller/AlbumImageController.java
@@ -1,5 +1,6 @@
 package com.jonathamjtm.gestaoartistas.controller;
 
+import com.jonathamjtm.gestaoartistas.service.AlbumService;
 import com.jonathamjtm.gestaoartistas.service.storage.FileStorageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -10,19 +11,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/v1/albums") //
+@RequestMapping("/api/v1/albums")
 @RequiredArgsConstructor
 @Tag(name = "Álbuns", description = "Gerenciamento de capas")
 @SecurityRequirement(name = "bearer-key")
 public class AlbumImageController {
 
     private final FileStorageService fileStorageService;
+    private final AlbumService albumService;
 
     @PostMapping(value = "/{id}/cover", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "Upload de Capa", description = "Envia a imagem da capa do álbum")
     public ResponseEntity<String> uploadCover(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
+
         String fileName = fileStorageService.upload(file);
         return ResponseEntity.ok("Imagem salva: " + fileName);
+    }
+
+    @GetMapping("/{id}/cover")
+    @Operation(summary = "Obter Capa", description = "Retorna uma URL temporária (30min) para baixar a imagem")
+    public ResponseEntity<Map<String, String>> getCoverUrl(@PathVariable Long id) {
+
+        String fileName = albumService.findImageNameByAlbumId(id);
+
+        String url = fileStorageService.getPresignedUrl(fileName);
+
+        return ResponseEntity.ok(Map.of("url", url));
     }
 }
