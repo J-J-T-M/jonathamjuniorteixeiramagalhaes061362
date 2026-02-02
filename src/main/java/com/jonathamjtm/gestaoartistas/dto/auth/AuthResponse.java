ca650b71bff5617fcf2d81eaf2dd8247491fb8f7
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-02 16:56:40 -0400

    feat: Adicionar campos refreshToken e type à classe AuthResponse

--- src/main/java/com/jonathamjtm/gestaoartistas/dto/auth/AuthResponse.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/dto/auth/AuthResponse.java
@@ -3,10 +3,14 @@ package com.jonathamjtm.gestaoartistas.dto.auth;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
     private String token;
+    private String refreshToken;
+    private String type = "Bearer";
 }
