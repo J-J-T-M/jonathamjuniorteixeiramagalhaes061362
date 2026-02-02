34651da53ef3ab3bbe70bf1b2d6c698b86cf939a
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-02 16:55:55 -0400

    feat: Adicionar construtores padrão e com argumentos à classe RegisterRequest

--- src/main/java/com/jonathamjtm/gestaoartistas/dto/auth/RegisterRequest.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/dto/auth/RegisterRequest.java
@@ -3,9 +3,13 @@ package com.jonathamjtm.gestaoartistas.dto.auth;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
     @NotBlank(message = "O nome completo é obrigatório")
     private String fullName;
