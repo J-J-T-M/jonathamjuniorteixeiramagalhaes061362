a100ab398e2e88a5b014414267a01e685a866213
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-01 23:07:36 -0400

    refactor: Simplificar anotações e melhorar a legibilidade da classe Artist

--- src/main/java/com/jonathamjtm/gestaoartistas/entity/Artist.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/entity/Artist.java
@@ -1,11 +1,7 @@
 package com.jonathamjtm.gestaoartistas.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -13,12 +9,15 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "artists")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = "albums")
 public class Artist {
 
     @Id
@@ -29,14 +28,12 @@ public class Artist {
     private String name;
 
     @ManyToMany(mappedBy = "artists", fetch = FetchType.LAZY)
-    @JsonIgnoreProperties("artists")
+    @Builder.Default
     private Set<Album> albums = new HashSet<>();
 
     @CreationTimestamp
-    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
-    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
