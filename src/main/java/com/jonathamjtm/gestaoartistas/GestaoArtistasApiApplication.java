b0c5826d5a5a3c6da6503bb4978a16c5af7cfbab
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-02 16:26:26 -0400

    feat: Adicionar bean RestTemplate para facilitar chamadas HTTP

--- src/main/java/com/jonathamjtm/gestaoartistas/GestaoArtistasApiApplication.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/GestaoArtistasApiApplication.java
@@ -2,14 +2,20 @@ package com.jonathamjtm.gestaoartistas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
 public class GestaoArtistasApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestaoArtistasApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoArtistasApiApplication.class, args);
+    }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
