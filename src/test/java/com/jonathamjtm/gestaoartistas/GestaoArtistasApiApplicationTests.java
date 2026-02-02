be5d02dc4a12af735c48f8ee68f4cf67d4210e82
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-01 23:15:33 -0400

    test: Adicionar perfil ativo de teste para os testes de aplicação

--- src/test/java/com/jonathamjtm/gestaoartistas/GestaoArtistasApiApplicationTests.java
+++ src/test/java/com/jonathamjtm/gestaoartistas/GestaoArtistasApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.jonathamjtm.gestaoartistas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestaoArtistasApiApplicationTests {
 
 	@Test
