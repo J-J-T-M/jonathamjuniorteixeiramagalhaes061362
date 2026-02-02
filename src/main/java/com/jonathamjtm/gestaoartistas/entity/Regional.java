5ec987c88454339a05c8f95433e1c9aa9089e074
J-J-T-M <jonatham.junior.18@gmail.com>
2026-02-02 01:35:35 -0400

    feat: Refatorar classe Regional para usar anotações de Lombok e adicionar EqualsAndHashCode

--- src/main/java/com/jonathamjtm/gestaoartistas/entity/Regional.java
+++ src/main/java/com/jonathamjtm/gestaoartistas/entity/Regional.java
@@ -1,33 +1,33 @@
 package com.jonathamjtm.gestaoartistas.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "regionals")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
 public class Regional {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "regional_id", nullable = false)
+    private Long regionalId;
 
     @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
-    private Boolean active;
+    private boolean active;
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
